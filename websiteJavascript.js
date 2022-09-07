f0433abd604ce480659b2f08378bdc933710373a
stefvlis <111745663+stefvlis@users.noreply.github.com>
2022-09-07 11:46:30 +0200

    Add files via upload

---
+++ websiteJavascript.js
@@ -0,0 +1,13 @@
+var toPay = 0;
+
+function addItem(x) {
+    toPay += x
+}
+
+function computeTotal() {
+    var paragraph = document.createElement("p"); 
+    var message = document.createTextNode("Your total is " + toPay.toFixed(2) + " euros");
+    paragraph.appendChild(message);
+    var betalenMessage = document.getElementById("toPay")
+    betalenMessage.appendChild(paragraph); 
+}
