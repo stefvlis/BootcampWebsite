51f6ef1a493e426871aaa613422a7d6c76b64c36
Stef <svdv@blue10.com>
2022-09-07 15:01:17 +0200

    added count column

--- websiteJavascript.js
+++ websiteJavascript.js
@@ -1,15 +1,22 @@
 var toPay = 0;
-const knop = document.getElementById("betaalKnop")
+var count = [0,0,0,0,0,0,0,0,0,0,0];
+const knop = document.getElementById("betaalKnop");
 
 function addItem(x) {
-    toPay += x
+    toPay += x;
 }
 
 function computeTotal() {
     var paragraph = document.createElement("p"); 
     var message = document.createTextNode("Your total is " + toPay.toFixed(2) + " euros");
     paragraph.appendChild(message);
-    var betalenMessage = document.getElementById("toPay")
+    var betalenMessage = document.getElementById("toPay");
     betalenMessage.appendChild(paragraph); 
-    knop.style.display = "none"
+    knop.style.display = "none";
+}
+
+function updateCount(id) {
+    count[id]+=1;
+    //let newCount =count[id];
+    document.getElementById(id).innerHTML = count[id];
 }
