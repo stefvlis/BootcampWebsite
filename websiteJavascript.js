0984f542ab3b54c2c8905d6a46c652f1f2fcf530
Stef <svdv@blue10.com>
2022-09-19 10:41:30 +0200

    Get menu from JSON file through azure function

--- websiteJavascript.js
+++ websiteJavascript.js
@@ -1,22 +1,104 @@
-var toPay = 0;
-var count = [0,0,0,0,0,0,0,0,0,0,0];
+// const cors = require("cors");
+// app.use(cors());
+let url = 'https://b10bc-weu-jsonreaderstef-fa.azurewebsites.net/api/HelloWorld'
+let menu;
 const knop = document.getElementById("betaalKnop");
+var toPay = 0;
 
-function addItem(x) {
-    toPay += x;
+function getJSON() {
+    fetch(url, { method: "GET" })
+        .then((response) => response.json())
+        .then((data) => {
+            menu = data;
+            console.log(menu);
+            printMenu(menu);
+        })
+        .catch((error) => { console.log(error) });
+}
+
+function printMenu(pMenu) {
+    var count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    var menuTable = document.getElementById("menuTable")
+
+    var tableRow = document.createElement("tr");
+
+    var idHeader = document.createElement("td");
+    var idHeaderText = document.createTextNode("ID");
+    idHeader.appendChild(idHeaderText);
+
+    var nameHeader = document.createElement("td");
+    var nameHeaderText = document.createTextNode("Item");
+    nameHeader.appendChild(nameHeaderText);
+
+    var priceHeader = document.createElement("td");
+    var priceHeaderText = document.createTextNode("Price");
+    priceHeader.appendChild(priceHeaderText);
+
+    var countHeader = document.createElement("td");
+    var countHeaderText = document.createTextNode("Count");
+    countHeader.appendChild(countHeaderText);
+
+    tableRow.appendChild(idHeader);
+    tableRow.appendChild(nameHeader);
+    tableRow.appendChild(priceHeader);
+    tableRow.appendChild(countHeader);
+    
+    menuTable.appendChild(tableRow);
+
+    pMenu.forEach(menuItem => {
+
+        var tableRow = document.createElement("tr");
+
+        var idTableData = document.createElement("td");
+        var idText = document.createTextNode(menuItem.id);
+        idTableData.appendChild(idText)
+
+        var nameTableData = document.createElement("td");
+        var nameText = document.createTextNode(menuItem.Dish);
+        nameTableData.appendChild(nameText)
+
+        var priceTableData = document.createElement("td");
+        var priceText = document.createTextNode(menuItem.Price);
+        priceTableData.appendChild(priceText);
+
+        var count = document.createElement("td");
+        var countText = document.createTextNode(0);
+        count.appendChild(countText);
+        count.id = menuItem.id
+        
+        var buttonInTable = document.createElement("td");
+        var button = document.createElement("input");
+        button.type = "button";
+        button.value = "Order";
+        button.id = menuItem.id
+        button.onclick = function() {addItem(pMenu[menuItem.id].Price); updateCount(menuItem.id);};
+        buttonInTable.appendChild(button);
+
+        tableRow.appendChild(idTableData);
+        tableRow.appendChild(nameTableData);
+        tableRow.appendChild(priceTableData);
+        tableRow.appendChild(count);
+        tableRow.appendChild(buttonInTable);
+
+        menuTable.appendChild(tableRow);
+    })
+
+    function addItem(x) {
+        toPay += x;
+    }
+
+    function updateCount(id) {
+        count[id] += 1;
+        document.getElementById(id).innerHTML = count[id];
+    }
 }
 
 function computeTotal() {
-    var paragraph = document.createElement("p"); 
+    var paragraph = document.createElement("p");
     var message = document.createTextNode("Your total is " + toPay.toFixed(2) + " euros");
     paragraph.appendChild(message);
     var betalenMessage = document.getElementById("toPay");
-    betalenMessage.appendChild(paragraph); 
+    betalenMessage.appendChild(paragraph);
     knop.style.display = "none";
-}
-
-function updateCount(id) {
-    count[id]+=1;
-    //let newCount =count[id];
-    document.getElementById(id).innerHTML = count[id];
 }
